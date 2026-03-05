5b672911f5857d5e7aa4051f9647cd99e18a66a2
Ilija Medan <ilija.medan@gmail.com>
2026-03-05 09:25:54 -0600

    change paths and chunking for accre

--- build_vac.py
+++ build_vac.py
@@ -31,7 +31,7 @@ def infer_chunk(base_dir, flux, ivar, theta, H, label_mean, label_std, scatter):
         optimizer='two-stage',
         grid_points=None,
         grid_range=None,
-        batch_size_bfgs=500,
+        batch_size_bfgs=50000,
     )
     return test_inferred_labels, test_label_covariances
 
@@ -39,11 +39,11 @@ def infer_chunk(base_dir, flux, ivar, theta, H, label_mean, label_std, scatter):
 
 if __name__ == '__main__':
     # load the block file
-    block_file = '/uufs/chpc.utah.edu/common/home/sdss51/sdsswork/mwm/spectro/astra/0.8.1/spectra/block/mwmStarBlock-0.8.1.h5'
+    block_file = '/data/stassun/medani/SDSS-V_data/DR20_data/mwmStarBlock-0.8.1.h5'
     bf = h5py.File(block_file, 'r')
 
     # load mwmAllStar to get the snr
-    allstar_file = '/uufs/chpc.utah.edu/common/home/sdss51/sdsswork/mwm/spectro/astra/0.8.1/summary/mwmAllStar-0.8.1.fits.gz'
+    allstar_file = '/data/stassun/medani/SDSS-V_data/DR20_data/mwmAllStar-0.8.1.fits.gz'
     allstar = Table.read(allstar_file, hdu=1)
 
     # need to sort allstar
@@ -69,46 +69,64 @@ if __name__ == '__main__':
     label_std = res['label_std']
     scatter = res['scatter']
 
-    chunksize = 10000
+    chunksize = 50000
     n_stars = len(idx_snr)
     n_batches = (n_stars + chunksize - 1) // chunksize
 
     inferred_labels = np.zeros((n_stars, 4))
     label_covariances = np.zeros((n_stars, 4, 4))
 
+    save_dir = '/data/stassun/medani/SDSS-V_data/DR20_data'
+
     for i in tqdm(range(n_batches), desc="allstar batches"):
         start_idx = i * chunksize
         end_idx = min((i + 1) * chunksize, n_stars)
+        # dont run if already done the chunk
+        if os.path.isfile(f'{save_dir}/dr20_clam_inferred_parameters_chunk_{i:03d}.npz'):
+            chunk_res = np.load(f'{save_dir}/dr20_clam_inferred_parameters_chunk_{i:03d}.npz')
+            inferred_labels[start_idx: end_idx] = chunk_res['inferred_labels']
+            label_covariances[start_idx: end_idx] = chunk_res['label_covariances']
+        else:
+            idx_chunk = idx_snr[start_idx: end_idx]
 
-        idx_chunk = idx_snr[start_idx: end_idx]
+            flux = bf['boss']['spectra']['flux'][idx_chunk]
+            cont = bf['boss']['spectra']['continuum'][idx_chunk]
+            ivar = bf['boss']['spectra']['ivar'][idx_chunk]
 
-        flux = bf['boss']['spectra']['flux'][idx_chunk]
-        cont = bf['boss']['spectra']['continuum'][idx_chunk]
-        ivar = bf['boss']['spectra']['ivar'][idx_chunk]
+            norm_flux = flux / cont
 
-        norm_flux = flux / cont
+            # Compute inverse variance for normalized flux
+            norm_ivar = cont**2 * ivar
 
-        # Compute inverse variance for normalized flux
-        norm_ivar = cont**2 * ivar
+            # Handle non-finite values
+            bad_pixels = (
+                ~np.isfinite(norm_flux)
+            |   ~np.isfinite(norm_ivar)
+            |   (norm_ivar <= 0)
+            |   (norm_flux <= 0)
+            |   (norm_flux >= 1.2)
+            )
 
-        # Handle non-finite values
-        bad_pixels = (
-            ~np.isfinite(norm_flux)
-        |   ~np.isfinite(norm_ivar)
-        |   (norm_ivar <= 0)
-        |   (norm_flux <= 0)
-        |   (norm_flux >= 1.2)
-        )
+            norm_flux = np.where(bad_pixels, 1.0, norm_flux)
+            norm_ivar = np.where(bad_pixels, 0.0, norm_ivar)
 
-        norm_flux = np.where(bad_pixels, 1.0, norm_flux)
-        norm_ivar = np.where(bad_pixels, 0.0, norm_ivar)
+            inferred_labels[start_idx: end_idx], label_covariances[start_idx: end_idx] = infer_chunk(
+                base_dir, norm_flux, norm_ivar, theta, H, label_mean, label_std, scatter)
+            
+            # save the chunk just in case
+            np.savez_compressed(f'{save_dir}/dr20_clam_inferred_parameters_chunk_{i:03d}.npz',
+                                inferred_labels=inferred_labels[start_idx: end_idx],
+                                label_covariances=label_covariances[start_idx: end_idx],
+                                sdss_id=allstar['sdss_id'][idx_chunk],
+                                telescope=allstar['telescope'][idx_chunk])
 
-        inferred_labels[start_idx: end_idx], label_covariances[start_idx: end_idx] = infer_chunk(
-            base_dir, norm_flux, norm_ivar, theta, H, label_mean, label_std, scatter)
-
-    # save the results
-    np.savez_compressed('clam_inferred_parameters.npz',
+    # save all the results
+    np.savez_compressed(f'{save_dir}/dr20_clam_inferred_parameters.npz',
                         inferred_labels=inferred_labels,
                         label_covariances=label_covariances,
                         sdss_id=allstar['sdss_id'][idx_snr],
                         telescope=allstar['telescope'][idx_snr])
+    
+    # clean up the chunks
+    for i in tqdm(range(n_batches), desc="deleting chunk files"):
+        os.remove(f'{save_dir}/dr20_clam_inferred_parameters_chunk_{i:03d}.npz')
