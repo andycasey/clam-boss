3735664dba0be7182c905f73bd4bfc55b96272c1
Ilija Medan <ilija.medan@gmail.com>
2026-03-24 11:08:16 -0500

    update script

--- build_vac.py
+++ build_vac.py
@@ -4,12 +4,16 @@ import h5py
 from astropy.table import Table
 from tqdm import tqdm
 import optax
+import jax.numpy as jnp
+from jax import jit
+import jax.nn as jnn
 
 from clam_boss.model import (
     compute_nmf_weights,
     load_ridge_model_npz,
     predict_with_ridge_npz,
     infer_labels,
+    build_design_matrix_batch_jax
 )
 
 
@@ -51,6 +55,18 @@ def infer_chunk(base_dir, flux, ivar, theta, H, label_mean, label_std, scatter):
     return test_inferred_labels, test_label_covariances
 
 
+@jit
+def spectra_rchi2(labels_std, theta_jnp, H_jnp, scatter_sq,
+                  flux_jnp, var_jnp, n_wavelengths):
+    design_matrix = build_design_matrix_batch_jax(labels_std)
+    W = jnn.softplus(design_matrix @ theta_jnp)
+    pred_flux = 1.0 - W @ H_jnp
+
+    total_var = var_jnp + scatter_sq
+    chi_sq = (flux_jnp - pred_flux)**2 / total_var
+    rchi2 =  0.5 * jnp.sum(chi_sq, axis=1) / n_wavelengths
+    return rchi2, pred_flux
+
 
 if __name__ == '__main__':
     # load the block file
@@ -84,26 +100,70 @@ if __name__ == '__main__':
     label_std = res['label_std']
     scatter = res['scatter']
 
+    theta_jnp = jnp.array(theta)
+    H_jnp = jnp.array(H)
+    scatter_sq = jnp.array(scatter)**2
+
     chunksize = 100000
     n_stars = len(idx_snr)
     n_batches = (n_stars + chunksize - 1) // chunksize
-
-    inferred_labels = np.zeros((n_stars, 4))
-    label_covariances = np.zeros((n_stars, 4, 4))
+    n_wavelengths = bf['boss']['spectra']['flux'][:10].shape[1]
 
     save_dir = '/data/stassun/medani/SDSS-V_data/DR20_data'
+
+    # create hdf5 file
+    with h5py.File(f'{save_dir}/dr20_clam_inferred_parameters_w_rchi2.h5', "w") as f:
+        f.create_dataset('inferred_labels',
+                        shape=(n_stars, 4),
+                        dtype='float64',
+                        compression="gzip",
+                        chunks=(25, 4))
+        f.create_dataset('label_covariances',
+                        shape=(n_stars, 4, 4),
+                        dtype='float64',
+                        compression="gzip",
+                        chunks=(25, 4, 4))
+        f.create_dataset('sdss_id',
+                        shape=(n_stars,),
+                        dtype=allstar['sdss_id'].dtype,
+                        compression="gzip",
+                        chunks=True)
+        f.create_dataset('telescope',
+                        shape=(n_stars,),
+                        dtype=allstar['telescope'].dtype,
+                        compression="gzip",
+                        chunks=True)
+        f.create_dataset('rchi2',
+                        shape=(n_stars,),
+                        dtype='float64',
+                        compression="gzip",
+                        chunks=True)
+        f.create_dataset('model_flux',
+                        shape=(n_stars, n_wavelengths),
+                        dtype='float32',
+                        compression="gzip",
+                        chunks=(25, n_wavelengths))
 
     for i in tqdm(range(n_batches), desc="allstar batches"):
         start_idx = i * chunksize
         end_idx = min((i + 1) * chunksize, n_stars)
+        idx_chunk = idx_snr[start_idx: end_idx]
         # dont run if already done the chunk
         if os.path.isfile(f'{save_dir}/dr20_clam_inferred_parameters_chunk_{i:03d}.npz'):
             chunk_res = np.load(f'{save_dir}/dr20_clam_inferred_parameters_chunk_{i:03d}.npz')
-            inferred_labels[start_idx: end_idx] = chunk_res['inferred_labels']
-            label_covariances[start_idx: end_idx] = chunk_res['label_covariances']
-        else:
-            idx_chunk = idx_snr[start_idx: end_idx]
+            inferred_labels_chunk = chunk_res['inferred_labels']
+            label_covariances_chunk = chunk_res['label_covariances']
+            rchi2_chunk = chunk_res['rchi2']
+            model_flux_chunk = chunk_res['model_flux']
 
+            with h5py.File(f'{save_dir}/dr20_clam_inferred_parameters_w_rchi2.h5', "a") as f:
+                f['inferred_labels'][start_idx:end_idx] = inferred_labels_chunk
+                f['label_covariances'][start_idx:end_idx] = label_covariances_chunk
+                f['sdss_id'][start_idx:end_idx] = allstar['sdss_id'][idx_chunk]
+                f['telescope'][start_idx:end_idx] = allstar['telescope'][idx_chunk]
+                f['rchi2'][start_idx:end_idx] = rchi2_chunk
+                f['model_flux'][start_idx:end_idx] = model_flux_chunk
+        else:
             flux = bf['boss']['spectra']['flux'][idx_chunk]
             cont = bf['boss']['spectra']['continuum'][idx_chunk]
             ivar = bf['boss']['spectra']['ivar'][idx_chunk]
@@ -125,22 +185,35 @@ if __name__ == '__main__':
             norm_flux = np.where(bad_pixels, 1.0, norm_flux)
             norm_ivar = np.where(bad_pixels, 0.0, norm_ivar)
 
-            inferred_labels[start_idx: end_idx], label_covariances[start_idx: end_idx] = infer_chunk(
+            inferred_labels_chunk, label_covariances_chunk = infer_chunk(
                 base_dir, norm_flux, norm_ivar, theta, H, label_mean, label_std, scatter)
+
+            flux_jnp = jnp.array(norm_flux)
+            var_jnp = 1.0 / jnp.maximum(norm_ivar, 1e-16)
+            labels_std = jnp.array((inferred_labels_chunk - label_mean) / label_std)
+
+            rchi2_res, pred_flux_res = spectra_rchi2(
+                labels_std, theta_jnp, H_jnp, scatter_sq,
+                flux_jnp, var_jnp, n_wavelengths)
+            rchi2_chunk = np.array(rchi2_res)
+            model_flux_chunk = np.array(pred_flux_res, dtype='float32')
+
+            with h5py.File(f'{save_dir}/dr20_clam_inferred_parameters_w_rchi2.h5', "a") as f:
+                f['inferred_labels'][start_idx:end_idx] = inferred_labels_chunk
+                f['label_covariances'][start_idx:end_idx] = label_covariances_chunk
+                f['sdss_id'][start_idx:end_idx] = allstar['sdss_id'][idx_chunk]
+                f['telescope'][start_idx:end_idx] = allstar['telescope'][idx_chunk]
+                f['rchi2'][start_idx:end_idx] = rchi2_chunk
+                f['model_flux'][start_idx:end_idx] = model_flux_chunk
             
             # save the chunk just in case
             np.savez_compressed(f'{save_dir}/dr20_clam_inferred_parameters_chunk_{i:03d}.npz',
-                                inferred_labels=inferred_labels[start_idx: end_idx],
-                                label_covariances=label_covariances[start_idx: end_idx],
+                                inferred_labels=inferred_labels_chunk,
+                                label_covariances=label_covariances_chunk,
                                 sdss_id=allstar['sdss_id'][idx_chunk],
-                                telescope=allstar['telescope'][idx_chunk])
-
-    # save all the results
-    np.savez_compressed(f'{save_dir}/dr20_clam_inferred_parameters.npz',
-                        inferred_labels=inferred_labels,
-                        label_covariances=label_covariances,
-                        sdss_id=allstar['sdss_id'][idx_snr],
-                        telescope=allstar['telescope'][idx_snr])
+                                telescope=allstar['telescope'][idx_chunk],
+                                rchi2=rchi2_chunk,
+                                model_flux=model_flux_chunk)
     
     # clean up the chunks
     for i in tqdm(range(n_batches), desc="deleting chunk files"):
