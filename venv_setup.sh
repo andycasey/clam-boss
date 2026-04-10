9ea0812bc721ecd3317282939cc24ce1d859aece
Ilija Medan <ilija.medan@gmail.com>
2026-04-10 14:23:38 -0500

    switch to uv

--- venv_setup.sh
+++ venv_setup.sh
@@ -1,7 +1,7 @@
-rm -Rf .venv/
-
-module purge
-module load python/3.11 uv
-uv venv --system-site-packages
-source .venv/bin/activate
-uv pip install numpy ipython matplotlib scikit-learn
+ml purge
+setup_accre_software_stack
+ml load python/3.11.5 cuda/12.6
+source /home/medani/clam_boss_env/bin/activate
+export UV_INDEX_STRATEGY="unsafe-best-match"
+export UV_CONSTRAINT="/cvmfs/soft.computecanada.ca/config/python/constraints.txt"
+uv sync --active
