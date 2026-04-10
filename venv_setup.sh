9920f22a0eec4506985cdc5c5329d754ba8ae6f7
Ilija Medan <ilija.medan@gmail.com>
2026-04-10 15:01:51 -0500

    add gpu option

--- venv_setup.sh
+++ venv_setup.sh
@@ -4,4 +4,4 @@ ml load python/3.11.5 cuda/12.6
 source /home/medani/clam_boss_env/bin/activate
 export UV_INDEX_STRATEGY="unsafe-best-match"
 export UV_CONSTRAINT="/cvmfs/soft.computecanada.ca/config/python/constraints.txt:$(pwd)/constraints-hpc.txt"
-uv sync --active
+uv sync --active --extra gpu
