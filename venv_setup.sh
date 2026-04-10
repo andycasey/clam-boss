210afd6c17d3718567942227fa143aedf42ae107
Ilija Medan <ilija.medan@gmail.com>
2026-04-10 14:28:41 -0500

    add constraint

--- venv_setup.sh
+++ venv_setup.sh
@@ -3,5 +3,5 @@ setup_accre_software_stack
 ml load python/3.11.5 cuda/12.6
 source /home/medani/clam_boss_env/bin/activate
 export UV_INDEX_STRATEGY="unsafe-best-match"
-export UV_CONSTRAINT="/cvmfs/soft.computecanada.ca/config/python/constraints.txt"
+export UV_CONSTRAINT="/cvmfs/soft.computecanada.ca/config/python/constraints.txt:$(pwd)/constraints-hpc.txt"
 uv sync --active
